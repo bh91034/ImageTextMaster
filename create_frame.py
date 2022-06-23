c033fa0ae947f88f09759d320523b0db04418642
Sunghoon Lee <bh91034@gmail.com>
2022-06-24 01:09:27 +0900

    In center frame, change textboxes to canvases

--- create_frame.py
+++ create_frame.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import END, scrolledtext
 from tkinter import ttk
 from turtle import bgcolor
+from PIL import ImageTk, Image
 
 # root window
 root = tk.Tk()
@@ -72,10 +73,49 @@ top_frm_btn_save_img.pack(side='right')
 mid_frm = tk.Frame(root)
 mid_frm.pack(padx=2, pady=2, fill='both', expand=True)
 
-txtbox = scrolledtext.ScrolledText(mid_frm, width=40, height=10)
-txtbox.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
-txtbox2 = scrolledtext.ScrolledText(mid_frm, width=40, height=10)
-txtbox2.grid(row=0, column=1, sticky=tk.E+tk.W+tk.N+tk.S)
+bg = ImageTk.PhotoImage(file='./images/a.png')
+left_canvas = tk.Canvas(mid_frm, bg='lightgray')
+left_canvas.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+#left_canvas.create_image(0,0, image=bg, anchor="nw")
+right_canvas = tk.Canvas(mid_frm, bg='lightgray')
+right_canvas.grid(row=0, column=1, sticky=tk.E+tk.W+tk.N+tk.S)
+#right_canvas.create_image(0,0, image=bg, anchor="nw")
+
+def resizer_size_calculate(img_w, img_h, canvas_w, canvas_h):
+    print ('########> w1=', img_w, ', h1=', img_h, ', w2=', canvas_w, ', h2=', canvas_h)
+    w_ratio = img_h/img_w
+    h_ratio = img_w/img_h
+    print ('########> ratio=', w_ratio, ', img_w=', img_w, ', img_h=', img_w*w_ratio)
+    # step 1
+    if canvas_w*w_ratio <= canvas_h:
+        return int(canvas_w), int(canvas_w*w_ratio)
+    else:
+        return int(canvas_h*h_ratio), int(canvas_h)
+
+# Reference :
+# - https://www.youtube.com/watch?v=xiGQD2J47nA
+# - https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course/blob/master/image_bg_resize.py
+# - https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course/
+def resizer(e):
+    global bg_resize, bg_left, new_bg_left, bg_right, new_bg_right
+    left_canvas.delete("all")
+    right_canvas.delete("all")
+    if e.width >= bg.width() and e.height >= bg.height():
+        left_canvas.create_image(0,0, image=bg, anchor="nw")
+        right_canvas.create_image(0,0, image=bg, anchor="nw")
+    else:
+        bg_resize = Image.open('./images/a.png')
+        w1, h1 = bg_resize.size
+        w, h = resizer_size_calculate(w1, h1, e.width, e.height)
+        bg_left = bg_resize.resize((w, h), Image.ANTIALIAS)
+        new_bg_left = ImageTk.PhotoImage(bg_left)
+        left_canvas.create_image(0,0, image=new_bg_left, anchor="nw")
+        bg_right = bg_resize.resize((w, h), Image.ANTIALIAS)
+        new_bg_right = ImageTk.PhotoImage(bg_right)
+        right_canvas.create_image(0,0, image=new_bg_right, anchor="nw")
+
+left_canvas.bind('<Configure>', resizer)
+
 
 # Added by Q&A from stackoverflow
 # https://stackoverflow.com/questions/72713209/how-to-keep-two-textboxes-the-same-size-when-resizing-window-with-python-tkinter
