27a18dfcfba0f7d9f15b58f9a0d12f30d48de457
Sunghoon Lee <bh91034@gmail.com>
2022-06-24 23:10:55 +0900

    Refine title of work folder selection dialog

--- ITM/Control/TopFrameControl.py
+++ ITM/Control/TopFrameControl.py
@@ -5,6 +5,6 @@ from ITM.Data.DataManager import DataManager
 def clickedChangeFolder():
     print ('[TopFrameControl] clickedChangeFolder() called!!...')
     from ITM.Frame.TopFrame import TopFrame
-    dir_path = filedialog.askdirectory(parent=TopFrame.root, title='Please select a directory', initialdir=DataManager.target_folder)
+    dir_path = filedialog.askdirectory(parent=TopFrame.root, title='작업할 폴더를 선택하세요', initialdir=DataManager.target_folder)
     print("##> dir_path : ", dir_path)
     print("##> TopFrame.root : ", TopFrame.root)
