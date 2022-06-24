e9c2d6fdb5f3b166ffbac1de681fb5c15b74df48
Sunghoon Lee <bh91034@gmail.com>
2022-06-24 23:36:13 +0900

    [interim] Work folder change : checking if selected folder has images

--- ITM/Control/ControlManager.py
+++ ITM/Control/ControlManager.py
@@ -1,3 +1,11 @@
+import glob
+
 class ControlManager:
     def __init__(self, data_manager):
         pass
+
+    @classmethod
+    def changedWorkFolder(cls, work_dir):
+        print ('[ControlManager.changedWorkFolder] work_dir=', work_dir)
+
+            
--- ITM/Control/TopFrameControl.py
+++ ITM/Control/TopFrameControl.py
@@ -1,6 +1,20 @@
+import glob
 import tkinter
+from tkinter import messagebox as mb
 from tkinter import filedialog
+from ITM.Control.ControlManager import ControlManager
 from ITM.Data.DataManager import DataManager
+
+def __checkWorkFolder(work_dir):
+    # check if image exists
+    ext = ['png', 'jpg', 'gif']
+    target_files = []
+    [target_files.extend(glob.glob(work_dir + '/' + '*.' + e)) for e in ext]
+    if len(target_files) == 0:
+        mb.showerror("에러", "해당 폴더에는 이미지 파일이 없습니다")
+        return False
+    else:
+        return True
 
 def clickedChangeFolder():
     print ('[TopFrameControl] clickedChangeFolder() called!!...')
@@ -8,3 +22,7 @@ def clickedChangeFolder():
     dir_path = filedialog.askdirectory(parent=TopFrame.root, title='작업할 폴더를 선택하세요', initialdir=DataManager.target_folder)
     print("##> dir_path : ", dir_path)
     print("##> TopFrame.root : ", TopFrame.root)
+    if __checkWorkFolder(dir_path):
+        ControlManager.changedWorkFolder(dir_path)
+    
+
