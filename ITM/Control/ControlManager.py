d4c48460201aa5f368b1b066cf99bfdfe910d492
Sunghoon Lee <bh91034@gmail.com>
2022-06-25 18:30:37 +0900

    Showing file name on top frame instead of folder name

--- ITM/Control/ControlManager.py
+++ ITM/Control/ControlManager.py
@@ -23,6 +23,9 @@ class ControlManager:
         MiddleFrame.resetCanvasImages(work_img)
         cls.work_file = work_img
 
+        # set new dir to 'TopFrame' at the label displaying work dir
+        TopFrame.changeWorkFile(work_img)
+
     @classmethod
     def changedWorkFolder(cls, work_dir):
         print ('[ControlManager.changedWorkFolder] work_dir=', work_dir)
@@ -30,17 +33,16 @@ class ControlManager:
         # set new dir to 'DataManager' and make DataManager to reload image list
         DataManager.reset(work_dir)
 
-        # set new dir to 'TopFrame' at the label displaying work dir
-        TopFrame.changeWorkFolder(work_dir)
-        
         # clear all data in 'remove tab' of 'LowFrame'
         LowFrame.resetRemoveTabData()
 
         # clear all data in 'write tab' of 'LowFrame'
         LowFrame.resetWriteTabData()
 
-        # TODO: change images in cavases of 'MiddleFrame' with the 1st image of new dir
+        # change images in cavases of 'MiddleFrame' with the 1st image of new dir
         work_file = DataManager.target_files[0]
         MiddleFrame.resetCanvasImages(work_file)
         cls.work_file = work_file
-            
+
+        # set new dir to 'TopFrame' at the label displaying work dir
+        TopFrame.changeWorkFile(work_file)
--- ITM/Frame/TopFrame.py
+++ ITM/Frame/TopFrame.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-
 from ITM.Control.TopFrameControl import clickedChangeFolder, clickedNextImage, clickedPrevImage
 from ITM.Data.DataManager import DataManager
 
@@ -9,7 +8,7 @@ from ITM.Data.DataManager import DataManager
 #------------------------------------------------------------------------------
 class TopFrame:
     root = None
-    label_curr_folder_name = None
+    label_curr_file_name = None
     def __init__(self, root):
         # top buttons frame
         top_frm = tk.Frame(root)
@@ -20,11 +19,12 @@ class TopFrame:
         btn_nxt_img = ttk.Button(top_frm, text='다음 이미지', command=clickedNextImage)
         btn_change_folder = ttk.Button(top_frm, text='폴더 변경', command=clickedChangeFolder)
 
-        # label : 현재 작업 영역
-        label_curr_folder_title = ttk.Label(top_frm, text='작업 폴더:')
-        label_curr_folder_name = ttk.Label(top_frm, text=DataManager.target_folder)
+        # label : 현재 작업 파일
+        from ITM.Control.ControlManager import ControlManager
+        label_curr_file_title = ttk.Label(top_frm, text='작업 파일:')
+        label_curr_file_name = ttk.Label(top_frm, text=ControlManager.work_file)
 
-        TopFrame.label_curr_folder_name = label_curr_folder_name
+        TopFrame.label_curr_file_name = label_curr_file_name
 
         # top buttons : '결과 저장'
         btn_save_img = ttk.Button(top_frm, text='결과 저장')
@@ -32,12 +32,12 @@ class TopFrame:
         btn_prv_img.pack(side='left')
         btn_nxt_img.pack(side='left')
         btn_change_folder.pack(side='left')
-        label_curr_folder_title.pack(side='left')
-        label_curr_folder_name.pack(side='left')
+        label_curr_file_title.pack(side='left')
+        label_curr_file_name.pack(side='left')
         btn_save_img.pack(side='right')
 
         TopFrame.root = top_frm
     
     @classmethod
-    def changeWorkFolder(cls, work_dir):
-        cls.label_curr_folder_name.config(text=work_dir)
+    def changeWorkFile(cls, work_file):
+        cls.label_curr_file_name.config(text=work_file)
