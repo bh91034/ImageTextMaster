4fd2a20beac58cf3d5b8d865b40eba357cb606fb
Sunghoon Lee <bh91034@gmail.com>
2022-06-26 13:34:52 +0900

    [final] EasyOCR text detection : done

--- create_frame.py
+++ create_frame.py
@@ -3,7 +3,6 @@ from ITM.Data.DataManager import DataManager
 from ITM.Control.ControlManager import ControlManager
 
 #
-#data = DataManager('C:/Users/javaes/workspace/easy-ocr/ImageTextMaster/images/')
 data = DataManager()
 control = ControlManager(data)
 root = FrameManager.init(control)
