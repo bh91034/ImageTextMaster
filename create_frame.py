172e63252a67de7929b6d127042bdb682318427d
Sunghoon Lee <bh91034@gmail.com>
2022-06-27 01:54:00 +0900

    [final] Applying __OUTPUT_FILES__ : done

--- create_frame.py
+++ create_frame.py
@@ -2,7 +2,6 @@ from ITM.Frame.FrameManager import FrameManager
 from ITM.Data.DataManager import DataManager
 from ITM.Control.ControlManager import ControlManager
 
-#
 data = DataManager()
 control = ControlManager()
 root = FrameManager.init()
