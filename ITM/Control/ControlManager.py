7b99e7828bb5f7c0645014e13825b08299020b0d
Sunghoon Lee <bh91034@gmail.com>
2022-06-28 22:45:46 +0900

    Refine code

--- ITM/Control/ControlManager.py
+++ ITM/Control/ControlManager.py
@@ -13,6 +13,10 @@ class ControlManager:
 
     @classmethod
     def changedWorkImage(cls, work_file):
+
+        # it should be first
+        cls.work_file = work_file
+
         print ('[ControlManager.changedWorkImage] work_img=', work_file)
         # clear all data in 'remove tab' of 'LowFrame'
         LowFrame.resetRemoveTabData()
@@ -22,7 +26,6 @@ class ControlManager:
 
         # change images in cavases of 'MiddleFrame' with the 1st image of new dir
         MiddleFrame.resetCanvasImages(work_file)
-        cls.work_file = work_file
 
         # set new dir to 'TopFrame' at the label displaying work dir
         TopFrame.changeWorkFile(work_file)
