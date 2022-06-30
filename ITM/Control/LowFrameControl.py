7d4de92622ca4fe18aea97d410eb9c3a9a790819
Sunghoon Lee <bh91034@gmail.com>
2022-07-01 00:54:38 +0900

    Papago translator API (open API)

--- ITM/Control/LowFrameControl.py
+++ ITM/Control/LowFrameControl.py
@@ -32,9 +32,47 @@ def clickedTranslate():
     # check availability
     if text is not None and len(text) > 0:
         translator = Translator()
-        result_text = translator.translate(text, dest='ko')
-        LowFrame.low_frm_text_right.reset([result_text.text])
+        g_result_text = translator.translate(text, dest='ko')
+        p_result_text = __translate_by_papago(text)
+        if p_result_text is not None:
+            result_text = "===> 구글 번역 결과 <===\n" + g_result_text.text + "\n\n===> 파파고 번역 결과 <===\n" + p_result_text
+            LowFrame.low_frm_text_right.reset([result_text])
+        else:
+            LowFrame.low_frm_text_right.reset([g_result_text.text])
     return
+
+# Reference : Papago API (pypapago API)
+# - https://wikidocs.net/35983
+# - https://www.dbility.com/560
+# - https://ansan-survivor.tistory.com/75
+# - https://developers.naver.com/apps/#/myapps/fV9b7SkB6EST_pBANjIB/overview
+# - https://developers.naver.com/docs/papago/papago-detectlangs-overview.md#%EC%96%B8%EC%96%B4-%EA%B0%90%EC%A7%80-%EA%B0%9C%EC%9A%94
+def __translate_by_papago(text):
+    import os
+    import sys
+    import urllib.request
+    import json
+    client_id = "fV9b7SkB6EST_pBANjIB" # 개발자센터에서 발급받은 Client ID 값
+    client_secret = "gUlhZt5TWd" # 개발자센터에서 발급받은 Client Secret 값
+    #encText = urllib.parse.quote("번역할내용")
+    encText = urllib.parse.quote(text)
+    data = "source=zh-CN&target=ko&text=" + encText
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        res = json.loads(response_body.decode('utf-8'))
+        from pprint import pprint
+        pprint(res)
+        result = res['message']['result']['translatedText']
+        return result
+    else:
+        print("Error Code:" + rescode)
+        return None
 
 def clickedCheckAll():
     from ITM.Frame.LowFrame import LowFrame
